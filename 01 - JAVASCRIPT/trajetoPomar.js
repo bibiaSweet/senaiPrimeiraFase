2b929ad1e2052999e4db63b3de94d48d48e99ce8
bibiaSweet <biapolizel5@gmail.com>
2026-04-23 22:16:57 -0300

    (add trajeto Pomar

---
+++ 01 - JAVASCRIPT/trajetoPomar.js
@@ -0,0 +1,8 @@
+let inicial, final, total 
+
+inicial = Number (prompt ("Quantidade inicial de laranjas:"))
+final = Number (prompt ("Quantidade final de laranjas:"))
+
+total = inicial - final 
+
+console.log ("Total de laranjas vendidas no dia são: " + total)
