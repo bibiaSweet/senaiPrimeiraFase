50a02911e63596500570009a9ea6160bc0310bc8
BEATRIZ LUIZA POLIZEL <beatriz_polizel@estudante.sesisenai.org.br>
2026-06-15 22:16:56 -0300

    alterações

--- 05 - REPETIÇÃO/script.js
+++ 05 - REPETIÇÃO/script.js
@@ -1,8 +1,19 @@
-let contador = 0
+let contador = 0 //inicialização 
+while (contador < 10) { //(cond repet)
+alert ("Contador:" + contador)
+contador ++ //contagem
+}  // voltar e testar novamente
 
-while (contador < 10) {
-alert ("Contador é menor que 10")
-contador ++
-}  
-// voltar e testar novamente
+let peso 
+
+peso = Number (prompt ("Digite o peso:"))
+if (peso>1.1 || peso < 0.9){
+    alert ("Fora torelância do peso")
+}
+
+let contador = 0 
+while (contador < 10){
+    //trampo
+    contador++
+}
 
